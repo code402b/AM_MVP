4882e9b3e69a38758c2c52836637447f1e4f187b
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-10 17:15:06 -0400

    clean up MET api request, add comments defining responses

--- server/API.js
+++ server/API.js
@@ -5,25 +5,28 @@ const waltWhitmanBaseURL = 'https://pafmon-walt-whitman-poems.p.rapidapi.com/poe
 const leavesOfGrassTitles = require('./APIdata.js');
 
 module.exports = {
-
   getMETobject(id) {
     const METObjectEndpoint = `${baseUrlMET}objects/${id}`;
     axios.get(METObjectEndpoint);
   },
+  // relevant response values:
+  // "primaryImage": "https://images.metmuseum.org/CRDImages/as/original/L37147_2011_575_3ab.jpg",
+  // "primaryImageSmall": "https://images.metmuseum.org/CRDImages/as/web-large/L37147_2011_575_3ab.jpg",
+  // "objectName": "Scholar's rock",
+  // "title": "Rock in the Form of a Fantastic Mountain",
+  // "culture": "China",
+  // "period": "Qing dynasty (1644–1911)",
 
-  // returns array of object ids:
   getMETquery(query, medium) {
     // &isHighlight=true, returns objects marked as significant by the museum
-    let METQueryEndpoint = `
-      ${baseUrlMET}search?
-        hasImages=true
-        &q=${query}
-    `;
-    if (medium) {
-      METQueryEndpoint = `${METQueryEndpoint}&medium=${medium}`;
-    }
+    const METQueryEndpoint = medium
+      ? `${baseUrlMET}search?hasImages=true&medium=${medium}&q=${query}`
+      : `${baseUrlMET}search?hasImages=true&q=${query}`;
     axios.get(METQueryEndpoint);
   },
+  // returns array of object ids:
+  // "total": 184,
+  //  "objectIDs": [...ids]
 
   getWaltWhitmanPoem() {
     const randomIndex = Math.floor(Math.random() * 400);
@@ -32,4 +35,7 @@ module.exports = {
     axios.get(waltWhitmanPoemEndpoint);
   },
 
+  // "index": "poem-name",
+  // "title": "Poem Name",
+  // "text": "poem text"
 };
