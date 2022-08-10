e3075ab116ea40829cf0f65ed88faa4e0ba4a50c
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-10 14:57:03 -0400

    modify controller export style

---
+++ database/controllers.js
@@ -0,0 +1,29 @@
+const MVP = require('./models.js');
+
+module.exports = {
+  getNotes() {
+    return MVP.find().exec();
+  },
+
+  addNoteOrUpdate(noteObj) {
+    return MVP.findOneAndUpdate(
+      {
+        user: noteObj.user,
+        text: noteObj.text,
+        date: noteObj.date,
+      },
+      {
+        user: noteObj.user,
+        media: noteObj.media,
+        mediaType: noteObj.mediaType,
+        text: noteObj.text,
+        date: noteObj.date,
+        favorite: noteObj.favorite,
+        collection: noteObj.collection,
+      },
+      {
+        upsert: true,
+      },
+    ).exec();
+  },
+};
