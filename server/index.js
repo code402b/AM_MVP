446751b56a0eef5f3ea07fc6db6d3e197cdaa63c
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-10 14:47:29 -0400

    add basic express configuration

--- server/index.js
+++ server/index.js
@@ -1,18 +1,16 @@
-Header
-MediaContainer
-DragDrop
-TextEntry
-Modal
-ImageSelector
-TextSelector
-ImageTypeInput
-SearchTermInput
-TextTypeInput
-TextSourceInput
-Footer
-Logo
-ActionButton
-ImgSpawnButton
-TextSpawnButton
-SaveNoteButton
-DiscardNoteButton
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 3000;
+
+// const addNote = require('../database/controllers.js');
+
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static(path.join(__dirname, '../client/dist')));
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`);
+});
