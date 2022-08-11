e94167a94f86266d7b7f862689a49758c029c534
Anthony McGovern <mcgovern.anthony@gmail.com>
2022-08-10 21:05:19 -0400

    add basic react-router code to power Userfront auth system

---
+++ client/src/components/Userfront.js
@@ -0,0 +1,19 @@
+import Userfront from '@userfront/react';
+
+Userfront.init('6nz4ydmn');
+
+const SignupForm = Userfront.build({
+  toolId: 'dbkarl',
+});
+
+const LoginForm = Userfront.build({
+  toolId: 'nmkdab',
+});
+
+const PasswordResetForm = Userfront.build({
+  toolId: 'onrlam',
+});
+
+const LogoutButton = Userfront.build({
+  toolId: 'anlbao',
+});
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,5 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+} from 'react-router-dom';
 
 import { MediaContainer, TextEntry, Footer } from './components/component_index.jsx';
 
@@ -7,12 +13,54 @@ const root = createRoot(document.getElementById('root'));
 
 function App() {
   return (
-    <>
-      <MediaContainer />
-      <TextEntry />
-      <Footer />
-    </>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/login">Login</Link>
+            </li>
+            <li>
+              <Link to="/reset">Reset</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+          </ul>
+        </nav>
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/reset" element={<PasswordReset />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </div>
+    </Router>
   );
+}
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function Login() {
+  return <h2>Login</h2>;
+}
+
+function PasswordReset() {
+  return <h2>Password Reset</h2>;
+}
+
+function Dashboard() {
+  return <h2>Dashboard</h2>;
+}
+
+function NotFound() {
+  return <h2>404 Page not found</h2>;
 }
 
 root.render(<App />);
